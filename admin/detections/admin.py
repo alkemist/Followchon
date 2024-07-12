92eb57071862951cdca7f0298ae1959854c2062b
Jaden ACHAIN <jac@originaldog.fr>
2024-07-12 17:58:52 +0200

    Improve admin

--- admin/detections/admin.py
+++ admin/detections/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.contrib import messages
 from django.utils.translation import ngettext
 
-from .models import Zone
-from .models import Family
 from .models import Detection
+from .models import Family
+from .models import Zone
 
 
 class ZoneAdmin(admin.ModelAdmin):
@@ -22,24 +22,38 @@ class FamilyAdmin(admin.ModelAdmin):
 class DetectionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Identification',
-         {'fields': ['family', 'score', 'verified', 'date', 'photo_file', 'image_tag'], 'classes': []}),
+         {'fields': ['family', 'score', 'date', 'photo_file', 'image_tag', 'verified'], 'classes': []}),
         ('Position', {'fields': ['zone', 'center_x', 'center_y', 'width', 'height'], 'classes': ['inline']}),
     ]
-    list_display = ['id', 'family', 'zone', 'verified', 'score', 'date', 'image_tag']
+    list_display = ['id', 'family', 'zone', 'score', 'verified', 'date', 'image_tag']
     search_fields = ['family', 'zone', 'date']
     ordering = ['-date']
-    list_filter = ['family', 'zone', 'date']
+    list_filter = ['family', 'zone', 'date', 'verified']
     readonly_fields = ['image_tag']
-    actions = ["make_verified"]
+    actions = ['mark_as_verified', 'mark_as_draft']
 
-    @admin.action(description="Mark selected detection as verified")
-    def make_verified(self, request, queryset):
+    @admin.action(description="Mark selected detections as verified")
+    def mark_as_verified(self, request, queryset):
         updated = queryset.update(verified=True)
         self.message_user(
             request,
             ngettext(
-                "%d detection was successfully marked as verified.",
-                "%d detections were successfully marked as verified.",
+                "%d detections was successfully marked as draft.",
+                "%d detections were successfully marked as draft.",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
+
+    @admin.action(description="Mark selected detections as draft")
+    def mark_as_draft(self, request, queryset):
+        updated = queryset.update(verified=False)
+        self.message_user(
+            request,
+            ngettext(
+                "%d detections was successfully marked as draft.",
+                "%d detections were successfully marked as draft.",
                 updated,
             )
             % updated,
